72f9f720a533704a486cf81aba18487cf01583df
毕殿卿 <bdq123@outlook.com>
2020-08-22 12:27:21 +0800

    添加项目文件。

---
+++ AspNetCoreUseMarkdown/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreUseMarkdown.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
